1f8eb22ef43e3bdd75061a2dc41ef095ebe6e866
Franciscoffv <franffv0809@gmail.com>
2024-10-29 14:39:27 -0600

    Main.java

--- ProyectoEstructuraDatos/src/proyectoestructuradatos/Main.java
+++ ProyectoEstructuraDatos/src/proyectoestructuradatos/Main.java
@@ -6,21 +6,31 @@ public class Main {
     public static void main(String[] args) {
         ColaReservas cola = new ColaReservas();
 
-        // Agregar reservas
         cola.encolar("El Principito");
         cola.encolar("1984");
         cola.encolar("Cien anios de soledad");
 
-        // Mostrar reservas
         System.out.println(cola.toString());
 
-        // Atender reservas
         String libroAtendido = cola.atender();
         if (libroAtendido != null) {
             System.out.println("Reserva atendida: " + libroAtendido);
         }
 
-        // Mostrar reservas después de atender
         System.out.println(cola.toString());
+        PilaHistorial historial = new PilaHistorial();
+
+        historial.agregarAlHistorial("El Principito");
+        historial.agregarAlHistorial("1984");
+        historial.agregarAlHistorial("Cien anios de soledad");
+
+        System.out.println("Historial de libros prestados:\n" + historial.toString());
+
+        System.out.println("Ultimo libro prestado: " + historial.verUltimoPrestado());
+
+        String libroEliminado = historial.eliminarDelHistorial();
+        System.out.println("Libro eliminado del historial: " + libroEliminado);
+
+        System.out.println("Historial actualizado de libros prestados:\n" + historial.toString());
     }
 }
