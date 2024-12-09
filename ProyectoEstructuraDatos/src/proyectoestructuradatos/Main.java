ee050d08f9462c631bcd82331606eaa7fc29e2b7
Franciscoffv <franffv0809@gmail.com>
2024-12-09 16:58:37 -0600

    Main.java

--- ProyectoEstructuraDatos/src/proyectoestructuradatos/Main.java
+++ ProyectoEstructuraDatos/src/proyectoestructuradatos/Main.java
@@ -5,24 +5,24 @@ package proyectoestructuradatos;
 public class Main {
     public static void main(String[] args) {
         ListaSimple ls = new ListaSimple();
-        ls.agregarLibro("Don Quijote");
-        ls.agregarLibro("Matar a un ruiseñor");
+        ls.agregarLibro("Don Quijote ");
+        ls.agregarLibro("Matar a un ruiseñor ");
         System.out.println(ls.toString());
         ls.eliminarLibro();
-        System.out.println("Lista después de eliminar un libro: " + ls.toString());
+        System.out.println("Lista despues de eliminar un libro: " + ls.toString());
         
         ListaDC ldc = new ListaDC();
-        ldc.agregarLibro("Frankenstein");
-        ldc.agregarLibro("Drácula");
+        ldc.agregarLibro("Frankenstein ");
+        ldc.agregarLibro("Dracula ");
         System.out.println(ldc.toString());
         ldc.eliminarLibro();
-        System.out.println("Lista Doble Circular después de eliminar un libro: " + ldc.toString());
+        System.out.println("Lista Doble Circular despues de eliminar un libro: " + ldc.toString());
         
         ColaReservas cola = new ColaReservas();
 
-        cola.encolar("El Principito");
-        cola.encolar("1984");
-        cola.encolar("Cien anios de soledad");
+        cola.encolar("El Principito ");
+        cola.encolar("1984 ");
+        cola.encolar("Cien anios de soledad ");
 
         System.out.println(cola.toString());
 
@@ -34,9 +34,9 @@ public class Main {
         System.out.println(cola.toString());
         PilaHistorial historial = new PilaHistorial();
 
-        historial.agregarAlHistorial("El Principito");
-        historial.agregarAlHistorial("1984");
-        historial.agregarAlHistorial("Cien anios de soledad");
+        historial.agregarAlHistorial("El Principito ");
+        historial.agregarAlHistorial("1984 ");
+        historial.agregarAlHistorial("Cien anios de soledad ");
 
         System.out.println("Historial de libros prestados:\n" + historial.toString());
 
@@ -46,5 +46,18 @@ public class Main {
         System.out.println("Libro eliminado del historial: " + libroEliminado);
 
         System.out.println("Historial actualizado de libros prestados:\n" + historial.toString());
+        
+        
+     ListaC listac = new ListaC();
+        
+        listac.agregar("Don Quijote");
+        listac.agregar("El Principito");
+        listac.agregar("Frankenstein");
+
+        System.out.println("Elementos de la lista: ");
+        listac.mostrarLista();
+
+        System.out.println("Elementos de la lista despues de eliminar: ");
+        listac.mostrarLista();
     }
 }
