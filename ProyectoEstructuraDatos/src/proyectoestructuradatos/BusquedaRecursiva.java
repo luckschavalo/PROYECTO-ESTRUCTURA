032ae6f9d621c23d11e39af20196d0eb2cb96529
Franciscoffv <franffv0809@gmail.com>
2024-12-16 19:44:33 -0600

    BusquedaRecursiva.java

---
+++ ProyectoEstructuraDatos/src/proyectoestructuradatos/BusquedaRecursiva.java
@@ -0,0 +1,15 @@
+package proyectoestructuradatos;
+
+public class BusquedaRecursiva {
+  public PosicionR buscarLibroRecursivo(PosicionR[] posiciones, String libroBuscado, int indice) {
+        if (indice >= posiciones.length) {
+            return null; 
+        }
+
+        if (posiciones[indice].getNombreLibro().equals(libroBuscado)) {
+            return posiciones[indice]; 
+        }
+
+        return buscarLibroRecursivo(posiciones, libroBuscado, indice + 1);
+    }
+}
